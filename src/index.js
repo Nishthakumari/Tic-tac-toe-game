cbd1dd1b44ab40c86ce4bbc4fe93ce093fc85dfd
Nishthakumari <knishtha0@gmail.com>
2022-06-09 15:55:44 +0530

    Added code for Showing the Past Moves

--- src/index.js
+++ src/index.js
@@ -60,6 +60,8 @@ import './index.css';
       }
     }
 
+   
+
     handleClick(i)
     {
       const history= this.state.history;
@@ -81,6 +83,18 @@ import './index.css';
       const history= this.state.history;
       const current= history[history.length-1];
       const winner= calculateWinner(current.squares);
+
+      const moves= history.map((step, move)=> {
+        const desc= move ? 
+        'Go to move #' + move :
+        'Go to game start'; 
+        return(
+          <li>
+            <button onClick={()=> this.jumpTo(move)}>{desc}</button>
+          </li>
+        );
+      })
+
       let status;
       if(winner){
         status= 'Winner: '+winner;
@@ -97,7 +111,7 @@ import './index.css';
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
